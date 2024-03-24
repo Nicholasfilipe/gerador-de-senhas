f61215cb9965632eaaf087cf0ef588310358848d
Nicholasfilipe <nicholaslondrina@gmail.com>
2024-03-24 15:36:22 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const sliderElement = document.querySelector("#slider")
+const button = document.querySelector("#button")
+
+const sizePassword = document.querySelector("#valor")
+const password = document.querySelector("#password")
+const containerPassword = document.querySelector("#container-password")
+
+
+let charset ="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789!@#$&"
+let newPassword = ""
+sizePassword.innerHTML = sliderElement.value
+
+slider.oninput = function() {
+ sizePassword.innerHTML= this.value
+}
+
+function generatePassword (){
+    let pass =''
+    for(let i = 0, n = charset.length; i < sliderElement.value; ++i){
+        pass += charset.charAt(Math.floor(Math.random() * n))
+        
+    }
+    containerPassword.classList.remove("hide")
+    password.innerHTML = pass
+    newPassword = pass
+}
+function copyPassword(){
+    alert("Senha copiada!😎😉")
+    navigator.clipboard.writeText(newPassword)
+}
